17b4f9fa9183a8da99486520eff8c0cf3298108c
EDEN <you8inpark@gmail.com>
2024-10-31 11:52:47 +0900

    Add: : result printing for basic arithmetic operations in Calculator classCalculator class with basic arithmetic operations

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -18,3 +18,20 @@ class Calculator {
         }
     }
 }
+
+let calculator = Calculator()
+
+let addResult = calculator.add(a: 5, b: 3)
+print(addResult)
+
+let substractResult = calculator.substract(a: 5, b: 3)
+print(substractResult)
+
+let multiplyResult = calculator.multiply(a: 5, b: 3)
+print(multiplyResult)
+
+if let divideResult = calculator.divide(a: 5, b: 0) {
+    print(divideResult)
+} else {
+    print("0으로 나눔")
+}
